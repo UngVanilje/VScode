dc94506abd8545bdfbc804c51fdf2d4ae6956287
UngVanilje <103105638+UngVanilje@users.noreply.github.com>
2023-02-14 11:58:03 +0100

    123

--- 05 - Form elementer og events/Opgaver/Opgave 5.3/tabs.js
+++ 05 - Form elementer og events/Opgaver/Opgave 5.3/tabs.js
@@ -1,14 +1,17 @@
 const tabs = document.querySelectorAll("h2 span");
 const contents = document.querySelectorAll("h1");
+const descriptions = document.querySelectorAll("p");
 
 tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
     for (let i = 0; i < tabs.length; i++) {
       tabs[i].style.backgroundColor = "";
       contents[i].style.display = "none";
+      descriptions[i].style.display = "none";
     }
     tab.style.backgroundColor = "lightgray";
     contents[index].style.display = "block";
+    descriptions[index].style.display = "block";
   });
 });
 
